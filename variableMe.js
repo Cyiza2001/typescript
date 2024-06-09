f845941712b14f2a1ca209695060220cb9fd8291
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-06-10 03:14:20 +0530

    another commit

---
+++ variableMe.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = " hey manyembua";
+console.log(greetings);
+var userId = 2233333333333333;
+userId.toFixed();
---
+++ variableMe.ts
@@ -0,0 +1,6 @@
+let greetings: string = " hey manyembua";
+console.log(greetings);
+let userId = 2233333333333333;
+userId.toFixed();
+
+export {};
