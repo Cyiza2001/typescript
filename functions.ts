cfc83b162ad789f6129c21f392096021c34c1004
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-06-10 03:34:49 +0530

    another commit

---
+++ functions.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+function signUpUser(name: string, email: string, password: string) {
+  return `${name} has ${email} as email and ${password} as a password`;
+}
+addTwo(5);
+getUpper("djibuji");
+signUpUser("baptizo", "kibwa", "kalisa");
